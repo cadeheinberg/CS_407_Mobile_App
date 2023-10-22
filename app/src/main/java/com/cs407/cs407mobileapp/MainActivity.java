75562c064c7f44620bd2998bdef4e94b329d217e
cadeheinberg <62829015+cadeheinberg@users.noreply.github.com>
2023-10-22 01:26:58 -0500

    login draft

--- app/src/main/java/com/cs407/cs407mobileapp/MainActivity.java
+++ app/src/main/java/com/cs407/cs407mobileapp/MainActivity.java
@@ -1,14 +1,63 @@
 package com.cs407.cs407mobileapp;
 
+import static androidx.core.content.ContextCompat.startActivity;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Login");
+        setButtonOnClickListeners();
+        this.sharedPreferences = getSharedPreferences("cs407mobileapp", Context.MODE_PRIVATE);
     }
+
+    private void setButtonOnClickListeners() {
+        Button buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editTextUsername =(EditText) findViewById(R.id.editTextUsername);
+                sharedPreferences.edit().putString("username", editTextUsername.getText().toString()).apply();
+                goToTasksActivity();
+            }
+        });
+    }
+
+    private void goToTasksActivity(){
+        Intent intent = new Intent(this, TasksActivity.class);
+        startActivity(intent);
+    }
+
+    private void goToCameraActivity(){
+        Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
+    }
+
+    private void goToGalleryActivity(){
+        Intent intent = new Intent(this, GalleryActivity.class);
+        startActivity(intent);
+    }
+
+    private void goToMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/cs407/cs407mobileapp/TasksActivity.java
@@ -0,0 +1,37 @@
+package com.cs407.cs407mobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class TasksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tasks);
+        getSupportActionBar().hide();
+    }
+
+    private void goToTasksActivity(){
+        Intent intent = new Intent(this, TasksActivity.class);
+        startActivity(intent);
+    }
+
+    private void goToCameraActivity(){
+        Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
+    }
+
+    private void goToGalleryActivity(){
+        Intent intent = new Intent(this, GalleryActivity.class);
+        startActivity(intent);
+    }
+
+    private void goToMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+}
