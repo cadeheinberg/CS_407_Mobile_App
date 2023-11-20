0e474335c75813e25863659be20cd91e762427a0
cadeheinberg <62829015+cadeheinberg@users.noreply.github.com>
2023-11-20 17:33:57 -0600

    basic camera working

--- app/src/main/java/com/cs407/SnapTask/CameraActivity.java
+++ app/src/main/java/com/cs407/SnapTask/CameraActivity.java
@@ -1,21 +1,53 @@
 package com.cs407.SnapTask;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class CameraActivity extends AppCompatActivity {
-    
+
+    private static final int REQUEST_CODE = 22;
+    Button buttonPicture;
+    ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
         //getSupportActionBar().hide();
         getSupportActionBar().setTitle("Camera");
+        buttonPicture = findViewById(R.id.cameraButton);
+        imageView = findViewById(R.id.cameraImageView);
+
+        buttonPicture.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK){
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }else{
+            Toast.makeText(this, "Error displaying photo", Toast.LENGTH_SHORT).show();
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
     @Override
