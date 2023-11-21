a496031515f5bcaf34b5be1ad09b6d9258a7b4d7
cadeheinberg <62829015+cadeheinberg@users.noreply.github.com>
2023-11-20 23:14:46 -0600

    better camera and gallery, working

--- app/src/main/java/com/cs407/SnapTask/CameraActivity.java
+++ app/src/main/java/com/cs407/SnapTask/CameraActivity.java
@@ -2,53 +2,166 @@ package com.cs407.SnapTask;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.pm.PackageManager;
+import android.util.Log;
+import com.google.common.util.concurrent.ListenableFuture;
 
-public class CameraActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
-    private static final int REQUEST_CODE = 22;
-    Button buttonPicture;
-    ImageView imageView;
+public class CameraActivity extends AppCompatActivity implements ImageAnalysis.Analyzer, View.OnClickListener {
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+
+    PreviewView previewView;
+    private ImageCapture imageCapture;
+    private Button bRecord;
+    private Button bCapture;
+
+    //could use any number for these
+    private static final int PERMISSIONS_REQUEST_CAMERA = 42;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-        //getSupportActionBar().hide();
-        getSupportActionBar().setTitle("Camera");
-        buttonPicture = findViewById(R.id.cameraButton);
-        imageView = findViewById(R.id.cameraImageView);
 
-        buttonPicture.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cameraIntent, REQUEST_CODE);
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA }, PERMISSIONS_REQUEST_CAMERA);
+        }
+
+        previewView = findViewById(R.id.previewView);
+        bCapture = findViewById(R.id.bCapture);
+        bRecord = findViewById(R.id.bRecord);
+        bRecord.setText("start recording"); // Set the initial text of the button
+
+        bCapture.setOnClickListener(this);
+        bRecord.setOnClickListener(this);
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                startCameraX(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
             }
-        });
+        }, getExecutor());
+
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK){
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
-            imageView.setImageBitmap(photo);
-            GalleryActivity.thumbnails.add(photo);
-        }else{
-            Toast.makeText(this, "Error displaying photo", Toast.LENGTH_SHORT).show();
-            super.onActivityResult(requestCode, resultCode, data);
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSIONS_REQUEST_CAMERA) {
+            // Checking whether user granted the permission or not.
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Showing the toast message
+                Toast.makeText(this, "Camera Permission Granted", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Toast.makeText(this, "Camera Permission Denied", Toast.LENGTH_SHORT).show();
+            }
         }
+    }
+
+    Executor getExecutor() {
+        return ContextCompat.getMainExecutor(this);
+    }
+
+    @SuppressLint("RestrictedApi")
+    private void startCameraX(ProcessCameraProvider cameraProvider) {
+        cameraProvider.unbindAll();
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+        Preview preview = new Preview.Builder()
+                .build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        // Image capture use case
+        imageCapture = new ImageCapture.Builder()
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                .build();
+
+        // Image analysis use case
+        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build();
+
+        imageAnalysis.setAnalyzer(getExecutor(), this);
+
+        //bind to lifecycle:
+        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview, imageCapture);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.bCapture) {
+            capturePhoto();
+        }
+    }
+
+    private void capturePhoto() {
+        File photoFile = null;
+        try {
+            photoFile = getNewPhotoFilePath();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(photoFile != null){
+            imageCapture.takePicture(new ImageCapture.OutputFileOptions.Builder(photoFile).build(), getExecutor(),
+                    new ImageCapture.OnImageSavedCallback() {
+                        @Override
+                        public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                            Toast.makeText(CameraActivity.this, "Photo has been saved", Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void onError(@NonNull ImageCaptureException exception) {
+                            Toast.makeText(CameraActivity.this, "Error saving photo", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+    }
+
+    private File getNewPhotoFilePath() throws IOException {
+        String timestamp = new SimpleDateFormat("YYYYMMDD_HHMMSS").format(new Date());
+        String imageName = "jpg_" + timestamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File imageFile = File.createTempFile(imageName, ".jpg", storageDir);
+        return imageFile;
     }
 
     @Override
@@ -92,5 +205,11 @@ public class CameraActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
-    
+
+    @Override
+    public void analyze(@NonNull ImageProxy image) {
+        // image processing here for the current frame
+        Log.d("TAG", "analyze: got the frame at: " + image.getImageInfo().getTimestamp());
+        image.close();
+    }
 }
